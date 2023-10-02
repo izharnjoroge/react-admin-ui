ae1be8b38d32146e3eefbdff69fdaff26680d30d
izharnjoroge <izharnjoroge@gmail.com>
2023-10-02 16:19:55 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/navbar/Navbar";
+import { Footer } from "./components/footer/Footer";
+import { Menu } from "./components/menu/Menu";
+import { Home } from "./pages/home/Home";
+import { Users } from "./pages/users/Users";
+import { Products } from "./pages/products/Products";
+import "./styles/global.scss";
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+    // {
+    //   path: "/login",
+    //   element: <Products />,
+    // },
+  ]);
 
-  return (
-    <div>Hello World</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
